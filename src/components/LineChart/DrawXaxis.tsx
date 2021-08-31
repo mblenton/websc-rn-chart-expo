3e418ca2505f0c7998797933b84645ebc928c25f
Milan Rakos <milan@netgen.io>
2021-08-31 13:15:52 +0200

    add new comments

--- src/components/LineChart/DrawXaxis.tsx
+++ src/components/LineChart/DrawXaxis.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { G, Line, Text } from 'react-native-svg';
-import * as d3 from 'd3-scale';
+import * as d3Scale from 'd3-scale';
 
 interface IDrawXaxis {
   width: number;
   height: number;
-  xScale: d3.ScaleLinear<number, number>;
+  xScale: d3Scale.ScaleLinear<number, number>;
 }
 
 export const DrawXaxis = ({
@@ -14,13 +14,19 @@ export const DrawXaxis = ({
   height,
 }: IDrawXaxis): JSX.Element => {
   const numberOfTicks = xScale.ticks().length;
+  // numberOfTicks = 16
   const ticks = xScale.ticks(numberOfTicks);
+  // ticks = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
 
   return (
     <G key="xAxis" fill="none" transform={`translate(35, ${height - 30})`}>
       <Line stroke="rgba(0,0,0,1)" x1="0" y1="0" x2={width - 80} y2="0" />
       {ticks.map(tick => {
         const xTick = xScale(tick);
+        // for tick = 0 xTick = 0
+        // for tick = 2 xTick = 19.88
+        // for tick = 4 xTick = 39.76
+        // for tick = 30 xTick = 298.20
         return (
           <G
             key={`xa_${tick}_0`}
--- src/components/LineChart/DrawYaxis.tsx
+++ src/components/LineChart/DrawYaxis.tsx
@@ -9,13 +9,19 @@ interface IDrawYaxis {
 
 export const DrawYaxis = ({ yScale, height }: IDrawYaxis): JSX.Element => {
   const numberOfTicks = yScale.ticks().length;
+  // numberOfTicks = 10
   const ticks = yScale.ticks(numberOfTicks);
+  // ticks = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
 
   return (
     <G key="yAxis" fill="none" transform="translate(30, 10)">
       <Line stroke="rgba(0,0,0,1)" x1="0" y1="0" x2="0" y2={height - 50} />
       {ticks.map(tick => {
         const yTick = yScale(tick);
+        // for tick = 0 yTick = 148.58
+        // for tick = 2 yTick = 132.07
+        // for tick = 4 yTick = 115.56
+        // for tick = 18 yTick = 0
         return (
           <G
             key={`ya_${tick}_0`}
